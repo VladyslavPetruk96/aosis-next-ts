a7d5691c0d1f0f21d037a1b60d44b4b3f4172a99
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2024-09-16 23:52:31 +0300

    layout & Nav improve

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import Logo from "@/app/_components/Logo";
-import { Navigation } from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 import type { Metadata } from "next";
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
 	subsets: ["latin"],
@@ -27,14 +26,13 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+				className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
 			>
-				<header>
-					<Logo />
-				</header>
-				<Navigation />
-				{children}
-				<footer>Copyright by Oasis</footer>
+				<Header />
+
+				<div className="flex-1 px-8 py-12">
+					<main className="mx-auto max-w-7xl">{children}</main>
+				</div>
 			</body>
 		</html>
 	);
