e0279733eb820294be710aa73d3bb00e318620e2
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2024-09-18 22:43:37 +0300

    Home page build

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
+import backgroundImage from "@/public/bg.png";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div>
-			<h1>The Wild Oasis. Welcome to paradise.</h1>
+		<div className="mt-24">
+			{/* eslint-disable-next-line @next/next/no-img-element */}
+			<Image
+				className="object-cover object-top"
+				src={backgroundImage}
+				placeholder="blur"
+				quality={80}
+				fill
+				alt="Mountains and forests with two cabins"
+			/>
 
-			<Link href="/cabins">Explore luxury cabins</Link>
+			<div className="relative z-10 text-center">
+				<h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+					Welcome to paradise.
+				</h1>
+				<Link
+					href="/cabins"
+					className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+				>
+					Explore luxury cabins
+				</Link>
+			</div>
 		</div>
 	);
 }
